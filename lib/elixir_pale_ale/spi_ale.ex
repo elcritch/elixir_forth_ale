6f72ef2a07aa393a9267aea862826605ad714fd1
Jaremy Creechley <creechley@gmail.com>
2018-09-13 00:53:42 -0600

    initial forthwith interaction ...

--- lib/elixir_pale_ale/spi_ale.ex
+++ lib/elixir_pale_ale/spi_ale.ex
@@ -7,7 +7,7 @@ defimpl HalIO, for: ForthAle.SPI do
   @spec read(self :: pid(), count :: non_neg_integer()) :: {:ok, binary} | {:error, term}
   def read(device, read_count) do
     Serial.write(device.pid, [read_count, device.spi_dev_name, "|spi-read"])
-    value = Serial.read(device.pid, 3) |> :binary.list_to_bin()
+    value = Serial.read(device.pid, read_count) |> :binary.list_to_bin()
 
     {:ok, value}
   end
@@ -24,8 +24,10 @@ defimpl HalIO, for: ForthAle.SPI do
   def xfer(device, value) do
     bytelst = :binary.bin_to_list(value)
     bytecnt = Enum.count(bytelst)
+
     Serial.write(device.pid, [bytelst, bytecnt, device.spi_dev_name, "|spi-write"])
-    [value] = Serial.read(device.pid, 1)
+    value = Serial.read(device.pid, bytecnt) |> :binary.list_to_bin()
+
     {:ok, value}
   end
 end
